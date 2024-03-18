3c065df54e4ab0589a67a8a094c77751dc602a82
SoumoteshBanarjee <soumotesh123@gmail.com>
2024-03-19 01:51:45 +0530

    committing problem

---
+++ code/codechef/LBCL11_problems_CLB058.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    char x[10];
+    
+    // Take user input in variable x
+    scanf("%s", x);
+    
+    // Print greeting
+    printf("Hello %s", x);
+    
+    
+}
