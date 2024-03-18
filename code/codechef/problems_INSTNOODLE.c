0df85616ecd4eaf26fbb616da4d6d97c41c2143c
SoumoteshBanarjee <soumotesh123@gmail.com>
2024-03-18 14:18:25 +0530

    1st Codechef

---
+++ code/codechef/problems_INSTNOODLE.c
@@ -0,0 +1,27 @@
+// Chef and Instant Noodles
+// Chef has invented 
+// 1
+// 1-minute Instant Noodles. As the name suggests, each packet takes exactly 
+// 1
+// 1 minute to cook.
+
+// Chef's restaurant has 
+// �
+// X stoves and only 
+// 1
+// 1 packet can be cooked in a single stove at any minute.
+
+// How many customers can Chef serve in 
+// �
+// Y minutes if each customer orders exactly 
+// 1
+// 1 packet of noodles?
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int a,b;
+    scanf("%d %d", &a, &b);
+    printf("%d",a*b);
+    return 0;
+}
